758487ceeac7a3622c6a052ebe251c5081a8402e
GETHOR2635 <162325255+GETHOR2635@users.noreply.github.com>
2025-01-04 17:01:17 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Writeline("Hello World!");
+        }
+
+    }
+}
